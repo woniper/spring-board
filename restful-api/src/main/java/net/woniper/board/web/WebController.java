f4980e764a7a42be67c27a3217b3167a30c73a81
woniper <leekw3747@gmail.com>
2015-08-26 19:40:35 +0900

    - swagger admin 사용자만 접근 가능하게 제한

--- restful-api/src/main/java/net/woniper/board/web/WebController.java
+++ restful-api/src/main/java/net/woniper/board/web/WebController.java
@@ -1,8 +1,10 @@
 package net.woniper.board.web;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,6 +20,12 @@ public class WebController {
         Map<String, String> map = new HashMap<>();
         map.put("title", "board");
         return map;
+    }
+
+    @RequestMapping(value = {"/", "/swagger"}, method = RequestMethod.GET)
+    @Secured(value = "ADMIN")
+    public ModelAndView index() {
+        return new ModelAndView("redirect:/swagger/index.html");
     }
 
 }
