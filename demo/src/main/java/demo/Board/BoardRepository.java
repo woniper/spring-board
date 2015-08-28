e727573a6e45eb01eb76fba47bfc4d578ecba6fa
woniper <leekw3747@gmail.com>
2015-08-28 18:16:20 +0900

    - .gitignore 파일 삭제 및 수정

--- demo/src/main/java/demo/Board/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package demo.Board;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Created by woniper on 15. 4. 27..
- */
-public interface BoardRepository extends JpaRepository<Board, Integer>{
-}
--- demo/src/main/java/demo/inject/Inject.java
+++
@@ -1,7 +0,0 @@
-package demo.inject;
-
-/**
- * Created by woniper on 15. 5. 2..
- */
-public interface Inject {
-}
