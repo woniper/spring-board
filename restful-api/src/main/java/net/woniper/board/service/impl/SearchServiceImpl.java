761a9090ae5d32b3ac72851d7d7848700106adbb
woniper <leekw3747@gmail.com>
2015-10-01 17:24:30 +0900

    - searchService 기능 완성

--- restful-api/src/main/java/net/woniper/board/service/impl/SearchServiceImpl.java
+++ restful-api/src/main/java/net/woniper/board/service/impl/SearchServiceImpl.java
@@ -1,7 +1,10 @@
 package net.woniper.board.service.impl;
 
 import net.woniper.board.domain.Board;
+import net.woniper.board.domain.User;
+import net.woniper.board.errors.support.BoardNotFoundException;
 import net.woniper.board.repository.BoardRepository;
+import net.woniper.board.repository.UserRepository;
 import net.woniper.board.service.SearchService;
 import net.woniper.board.support.dto.SearchDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +18,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class SearchServiceImpl implements SearchService {
 
+    @Autowired private UserRepository userRepository;
     @Autowired private BoardRepository boardRepository;
 
     @Override
     public Page<Board> search(SearchDto searchDto, Pageable pageable) {
-        // todo querydsl로 만들기
-        return null;
+        Page<Board> boards = null;
+        switch (searchDto.getType()) {
+            case WRITER:
+                User user = userRepository.findByNickName(searchDto.getQuery());
+                boards = boardRepository.findByUser(user, pageable);
+                break;
+            case TITLE:
+                boards = boardRepository.findByTitleLike(searchDto.getLikeQuery(), pageable);
+                break;
+            case CONTENT:
+                boards = boardRepository.findByContentLike(searchDto.getLikeQuery(), pageable);
+                break;
+            case ALL:
+                boards = boardRepository.findByTitleLikeOrContentLike(searchDto.getLikeQuery(), searchDto.getLikeQuery(), pageable);
+                break;
+        }
+
+        if(boards == null)
+            throw new BoardNotFoundException(0L);
+
+        return boards;
     }
 }
