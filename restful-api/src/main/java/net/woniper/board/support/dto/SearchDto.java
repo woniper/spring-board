c5818d533ee8f9e06fb15654b5d354bda3cb10e9
woniper <leekw3747@gmail.com>
2015-10-03 15:11:08 +0900

    lombok annotation 변경

--- restful-api/src/main/java/net/woniper/board/support/dto/SearchDto.java
+++ restful-api/src/main/java/net/woniper/board/support/dto/SearchDto.java
@@ -1,12 +1,15 @@
 package net.woniper.board.support.dto;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import net.woniper.board.support.type.SearchType;
 
 /**
  * Created by woniper on 15. 9. 24..
  */
-@Data
+@AllArgsConstructor
+@Getter @Setter
 public class SearchDto {
 
     private SearchType type = SearchType.ALL;
