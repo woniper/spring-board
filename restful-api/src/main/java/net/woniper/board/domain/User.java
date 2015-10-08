3bfa0fd9e480ea0b9ae2441ba017dd8337b3c7ba
woniper <leekw3747@gmail.com>
2015-10-08 14:50:17 +0900

    user password toString()에서 제외 (보안)

--- restful-api/src/main/java/net/woniper/board/domain/User.java
+++ restful-api/src/main/java/net/woniper/board/domain/User.java
@@ -19,7 +19,7 @@ import java.util.List;
  */
 @Entity(name = "user")
 @Getter @Setter
-@ToString(exclude = "boards")
+@ToString(exclude = {"boards", "password"})
 @Slf4j
 public class User implements Serializable {
 
