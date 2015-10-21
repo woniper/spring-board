6f66fd5ac24ddd7c22241044c567ed9def7a27ff
woniper <leekw3747@gmail.com>
2015-10-21 22:23:53 +0900

    create Web-Vaadin module

---
+++ web-vaadin/src/test/java/net/woniper/vaadin/WebVaadingApplicationTests.java
@@ -0,0 +1,18 @@
+package net.woniper.vaadin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WebVaadinApplication.class)
+@WebAppConfiguration
+public class WebVaadingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
